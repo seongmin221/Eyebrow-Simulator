7e59c11f00fbd3ba8a0f6b0443543403890e5a6e
seongmin221 <seongmin221@naver.com>
2023-10-11 15:42:40 +0900

    [SETTING] applied needed setting to show view on viewcontroller

--- Eyebrow-Simulator/Eyebrow-Simulator/Feature/Camera/ViewController/CameraViewController.swift
+++ Eyebrow-Simulator/Eyebrow-Simulator/Feature/Camera/ViewController/CameraViewController.swift
@@ -5,8 +5,47 @@
 //  Created by 이성민 on 10/7/23.
 //
 
-import UIKit
+import Foundation
+import Combine
 
-final class CameraViewController: UIViewController {
+final class CameraViewController: BaseViewControllerType {
+    
+    // MARK: - Property
+    
+    var viewModel: CameraViewModel
+    var cancelBag: Set<AnyCancellable> = Set()
+    
+    // MARK: - UI Property
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    // MARK: - Setting
+    
+    
+    
+    // MARK: - Action Helper
+    
+    
+    
+    // MARK: - Custom Method
+    
+    init(_ viewModel: CameraViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind(viewModel: CameraViewModel) {
+        
+    }
+    
     
 }
